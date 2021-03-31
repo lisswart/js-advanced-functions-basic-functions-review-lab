e256cf975800fa50a4b5de669b1b36e13263003c
lisswart <arnoudlisa@gmail.com>
2021-03-30 22:05:57 -0400

    anonymous functions, scope chain

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+const saturdayFun = function(act = 'roller-skate') {
+    return `This Saturday, I want to ${act}!`;
+}
+
+const mondayWork = function(act = 'go to the office') {
+    return `This Monday, I will ${act}.`;
+}
+
+function wrapAdjective(style='*') {
+    return function(adj='special') {
+        return `You are ${style}${adj}${style}!`;
+    }
+}
+
+
+
