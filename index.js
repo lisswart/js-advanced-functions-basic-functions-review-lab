d2deffc159a8bf96990621e0e18efbf0d54adf61
lisswart <arnoudlisa@gmail.com>
2021-03-30 23:57:27 -0400

    figured out how to add fn to an object with destructuring

--- index.js
+++ index.js
@@ -13,5 +13,31 @@ function wrapAdjective(style='*') {
     }
 }
 
+const Calculator = {function: 'add'};
+Calculator.add = function(num1, num2) {
+    return (num1 + num2);
+}
 
+// const sum = Calculator.add(1,3);
+// console.log(sum);
 
+Calculator.subtract = function(num1, num2) {
+    return num1 - num2;
+}
+
+// const diff = Calculator.subtract(3, 1);
+// console.log(diff);
+
+Calculator.divide = function(num1, num2) {
+    return num1 / num2;
+}
+
+// const quotient = Calculator.divide(10,5);
+// console.log(quotient);
+
+Calculator.multiply = function(num1, num2) {
+    return num1 * num2;
+}
+
+const product = Calculator.multiply(1.5, 2);
+console.log(product);
