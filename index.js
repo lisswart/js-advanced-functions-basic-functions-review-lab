4296565b908a0f376c99cb61aa3e3d110918f912
lisswart <arnoudlisa@gmail.com>
2021-04-01 16:34:26 -0400

    commit tree 70d8b3931e9b3e13fa2653d55fbbacae8e384597

--- index.js
+++ index.js
@@ -13,44 +13,29 @@ function wrapAdjective(style='*') {
     }
 }
 
-// let encouragingPromptFunction = wrapAdjective('#####')('a dedicated programmer');
-// console.log(encouragingPromptFunction);
-
 const Calculator = {function: 'add'};
 Calculator.add = function(num1, num2) {
     return (num1 + num2);
 }
 
-// const sum = Calculator.add(1,3);
-// console.log(sum);
-
 Calculator.subtract = function(num1, num2) {
     return num1 - num2;
 }
-
-// const diff = Calculator.subtract(3, 1);
-// console.log(diff);
 
 Calculator.divide = function(num1, num2) {
     return num1 / num2;
 }
 
-// const quotient = Calculator.divide(10,5);
-// console.log(quotient);
-
 Calculator.multiply = function(num1, num2) {
     return num1 * num2;
 }
 
-// const product = Calculator.multiply(1.5, 2);
-// console.log(product);
-
-const arrayOfTransforms = [
-    function(num){ return num * 2 },
-    function(num){ return num + 1000},
-    function(num){ return num % 7 }
-  ];
-
+// const arrayOfTransforms = [
+//     function(num){ return num * 2 },
+//     function(num){ return num + 1000},
+//     function(num){ return num % 7 }
+//   ];
+ 
 function actionApplyer(num, arr) {
     
     if(arr.length === 0) {
@@ -61,10 +46,16 @@ function actionApplyer(num, arr) {
         // const sum = arr[1](product);
         // const modulo = arr[2](sum);
         // return modulo;
-        return arr[2](arr[1](arr[0](num)));        
+
+        return arr[2](arr[1](arr[0](num)));
+        
+        // const [product, sum, modulo] = (num) => {return [arr[2](arr[1](arr[0](num)))];};
+        // return modulo;
     }
 }
 
 console.log(actionApplyer(13, arrayOfTransforms));
+
+
 
 
